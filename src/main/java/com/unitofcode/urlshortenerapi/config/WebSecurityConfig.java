07ee74d9839faa5d33381f56800c981b06853c6b
Siddartha Boddu <siddarthasiddu96@gmail.com>
2020-02-25 21:27:37 +0530

    Update WebSecurityConfig.java

--- src/main/java/com/unitofcode/urlshortenerapi/config/WebSecurityConfig.java
+++ src/main/java/com/unitofcode/urlshortenerapi/config/WebSecurityConfig.java
@@ -31,7 +31,7 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration.applyPermitDefaultValues());
